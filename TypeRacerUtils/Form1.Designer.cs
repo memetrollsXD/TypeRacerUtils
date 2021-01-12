ad875decfe5f58e2df3ba4e3a2653b6daea50895
memetrollsXD <memetrollsXD@DESKTOP-O2IEOKA>
2021-01-12 15:58:35 +0100

    First Commit

---
+++ TypeRacerUtils/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace TypeRacerUtils
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hookBtn = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.shortBox = new System.Windows.Forms.CheckBox();
+            this.capsBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // hookBtn
+            // 
+            this.hookBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.8F);
+            this.hookBtn.Location = new System.Drawing.Point(12, 12);
+            this.hookBtn.Name = "hookBtn";
+            this.hookBtn.Size = new System.Drawing.Size(360, 54);
+            this.hookBtn.TabIndex = 0;
+            this.hookBtn.Text = "Unhook";
+            this.hookBtn.UseVisualStyleBackColor = true;
+            this.hookBtn.Click += new System.EventHandler(this.hookBtn_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(12, 358);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(360, 22);
+            this.outputBox.TabIndex = 1;
+            // 
+            // shortBox
+            // 
+            this.shortBox.AutoSize = true;
+            this.shortBox.Checked = true;
+            this.shortBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.shortBox.Location = new System.Drawing.Point(12, 72);
+            this.shortBox.Name = "shortBox";
+            this.shortBox.Size = new System.Drawing.Size(305, 21);
+            this.shortBox.TabIndex = 2;
+            this.shortBox.Text = "Disable Save and Print Shortcuts (CTRL+S)";
+            this.shortBox.UseVisualStyleBackColor = true;
+            // 
+            // capsBox
+            // 
+            this.capsBox.AutoSize = true;
+            this.capsBox.Checked = true;
+            this.capsBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.capsBox.Location = new System.Drawing.Point(12, 99);
+            this.capsBox.Name = "capsBox";
+            this.capsBox.Size = new System.Drawing.Size(242, 21);
+            this.capsBox.TabIndex = 3;
+            this.capsBox.Text = "Disable Caps Lock (Experimental)";
+            this.capsBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 392);
+            this.Controls.Add(this.capsBox);
+            this.Controls.Add(this.shortBox);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.hookBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TypeRacer Utils";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button hookBtn;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.CheckBox shortBox;
+        private System.Windows.Forms.CheckBox capsBox;
+    }
+}
+
---
+++ TypeRacerUtils/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Utilities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace TypeRacerUtils
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            gkh.HookedKeys.Add(Keys.LControlKey);
+            gkh.HookedKeys.Add(Keys.RControlKey);
+            gkh.HookedKeys.Add(Keys.S);
+            gkh.HookedKeys.Add(Keys.P);
+            gkh.HookedKeys.Add(Keys.A);
+            gkh.HookedKeys.Add(Keys.CapsLock);
+            gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+            gkh.KeyUp += new KeyEventHandler(gkh_KeyUp);
+            hooked = true;
+            Print("Successfully Hooked!");
+        }
+        [DllImport("user32.dll")]
+static extern void keybd_event(byte bVk, byte bScan, uint dwFlags,
+UIntPtr dwExtraInfo);
+        globalKeyboardHook gkh = new globalKeyboardHook();
+        public bool hooked;
+        private void hookBtn_Click(object sender, EventArgs e)
+        {
+            if (!hooked)
+            {
+                gkh.hook();
+                hooked = true;
+                hookBtn.Text = "Unhook";
+                Print("Successfully Hooked!");
+            }
+            else
+            {
+                gkh.unhook();
+                hooked = false;
+                hookBtn.Text = "Hook";
+                Print("Successfully Unhooked!");
+            }
+        }
+        public bool hold;
+        void gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (shortBox.Checked)
+            {
+                if (e.KeyCode.ToString() == "LControlKey" | e.KeyCode.ToString() == "RControlKey")
+                {
+                    hold = true;
+                }
+                if(hold) {
+                    if (e.KeyCode.ToString() == "S" | e.KeyCode.ToString() == "P")
+                    {
+                        e.Handled = true;
+                    }
+                }
+            }
+            if (capsBox.Checked)
+            {
+                if (Control.IsKeyLocked(Keys.CapsLock)) // Checks Capslock is on
+                {
+                    const int KEYEVENTF_EXTENDEDKEY = 0x1;
+                    const int KEYEVENTF_KEYUP = 0x2;
+                    keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY, (UIntPtr)0);
+                    keybd_event(0x14, 0x45, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,
+                    (UIntPtr)0);
+                }
+            }
+        }
+        void gkh_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (shortBox.Checked)
+            {
+                if (e.KeyCode.ToString() == "LControlKey" | e.KeyCode.ToString() == "RControlKey")
+                {
+                    hold = false;
+                }
+            }
+        }
+        void Print(string str)
+        {
+            outputBox.Text = str;
+        }
+    }
+}
